0532cbe92390f17b49356a5862fad72b3f1d73d9
jungwoo <okjwoo@ajou.ac.kr>
2018-12-09 13:06:06 +0900

    프로그램입력

--- routes/menu.js
+++ routes/menu.js
@@ -18,7 +18,26 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
 
     console.log(req.body);
-    res.send({success:true});
+    // res.send({success:true});
+
+    pool.getConnection(function(err,connection){
+
+        connection.query('select * from Requirements where P_Name=? AND Specification=?', [req.body.program, req.body.spec], function (err, rows) {
+            if(err){ connection.release(); }
+
+            console.log(rows);
+
+            if(rows.length == 0) {
+                console.log('검색 실패');
+                res.json({success:false});
+            } else {
+                console.log('검색 성공');
+                // res.send({success:true});
+                res.send({success:true, info:rows[0]});
+            }
+            connection.release();
+        });
+    });
 });
 
 module.exports = router;
